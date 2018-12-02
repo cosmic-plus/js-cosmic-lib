b99b1f585a02ce75f47aa480b6e40fad0a7d2d94
Antoine <Mister.Ticot@gmail.com>
2018-12-02 12:38:03 -0500

    Move part of src/load.js into jsutils

--- src/load.js
+++ src/load.js
@@ -1,15 +1,12 @@
 "use_strict"
 /**
  * Asynchronously load external ressources.
- *
- * @exports load
  */
-const load = exports
-
-const dom = require("@cosmic-plus/jsutils/dom")
-const html = require("@cosmic-plus/jsutils/html")
+const parent = require("@cosmic-plus/jsutils/load")
+const load = module.exports
 
 /**
+ *
  * Load cosmic-lib CSS from **href** or from default origin. All subsequent
  * calls will refer to the original request.
  *
@@ -21,32 +18,15 @@ const html = require("@cosmic-plus/jsutils/html")
  * /// description:
  * await cosmicLib.load.styles()
  *
+ * @alias module:load#styles
  * @async
  * @param {string} [href='cosmic-lib.css']
  */
 load.styles = async function (conf, href = "https://cosmic.plus/cosmic-lib/cosmic-lib.css") {
-  const promise = load.css(conf, href)
+  const promise = parent.css(href)
   load.styles = () => promise
   return promise
 }
 
-/**
- * Load a CSS from **href**.
- *
- * @example
- * /// Preload at script initialization:
- * const cssLoader = cosmicLib.load.css('my-styles.css')
- *
- * /// To wait for loading to finish:
- * await cssLoader
- *
- * @async
- * @param {string} href
- */
-load.css = async function (conf, href) {
-  return new Promise(function (resolve, reject) {
-    const attributes = { rel: "stylesheet", type: "text/css", href: href, onload: resolve, onerror: reject }
-    const linkNode = html.create("link", attributes)
-    html.append(dom.head, linkNode)
-  })
-}
+// Make other member compatible with how cosmic-lib exposes modules.
+load.css = (conf, href) => parent.css(href)
